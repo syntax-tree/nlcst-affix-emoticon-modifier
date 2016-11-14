63789be429f5cc0e285e3a4785ed27aac2bbbd06
Titus Wormer <tituswormer@gmail.com>
2016-11-14 19:29:21 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,78 +1,47 @@
-/**
- * @author Titus Wormer
- * @copyright 2014-2015 Titus Wormer
- * @license MIT
- * @module nlcst:affix-emoticon-modifier
- * @fileoverview Merge affix emoticons into the previous sentence in NLCST.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
-
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var modifier = require('unist-util-modify-children');
 
-/*
- * Constants: node types.
- */
+/* Expose. */
+module.exports = modifier(mergeAffixEmoticon);
 
+/* Constants: node types. */
 var EMOTICON_NODE = 'EmoticonNode';
 
-/**
- * Merge emoticons into an `EmoticonNode`.
- *
- * @param {CSTNode} child - Node to check.
- * @param {number} index - Position of `child` in `parent`.
- * @param {CSTNode} parent - Parent of `child`.
- * @return {number?} - Either void, or the next index to
- *   iterate over.
- */
+/* Merge emoticons into an `EmoticonNode`. */
 function mergeAffixEmoticon(child, index, parent) {
-    var children = child.children;
-    var position;
-    var node;
-    var prev;
+  var children = child.children;
+  var position;
+  var node;
+  var prev;
 
-    if (children && children.length && index !== 0) {
-        position = -1;
+  if (children && children.length !== 0 && index !== 0) {
+    position = -1;
 
-        while (children[++position]) {
-            node = children[position];
+    while (children[++position]) {
+      node = children[position];
 
-            if (node.type === EMOTICON_NODE) {
-                prev = parent.children[index - 1];
+      if (node.type === EMOTICON_NODE) {
+        prev = parent.children[index - 1];
 
-                prev.children = prev.children.concat(
-                    children.slice(0, position + 1)
-                );
+        prev.children = prev.children.concat(
+          children.slice(0, position + 1)
+        );
 
-                child.children = children.slice(position + 1);
+        child.children = children.slice(position + 1);
 
-                if (node.position && child.position && prev.position) {
-                    prev.position.end = child.position.start =
-                        node.position.end;
-                }
-
-                /*
-                 * Next, iterate over the node again.
-                 */
-
-                return index;
-            }
-
-            if (node.type !== 'WhiteSpaceNode') {
-                break;
-            }
+        if (node.position && child.position && prev.position) {
+          prev.position.end = child.position.start = node.position.end;
         }
+
+        /* Next, iterate over the node again. */
+        return index;
+      }
+
+      if (node.type !== 'WhiteSpaceNode') {
+        break;
+      }
     }
+  }
 }
-
-/*
- * Expose.
- */
-
-module.exports = modifier(mergeAffixEmoticon);
--- test/index.js
+++ test/index.js
@@ -1,11 +1,8 @@
 'use strict';
 
-/* eslint-env node, mocha */
+/* eslint-env mocha */
 
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var assert = require('assert');
 var retext = require('retext');
 var english = require('retext-english');
@@ -13,86 +10,33 @@ var emojiModifier = require('nlcst-emoji-modifier');
 var emoticonModifier = require('nlcst-emoticon-modifier');
 var modifier = require('..');
 
-/*
- * Methods.
- */
-
+/* Methods. */
 var dequal = assert.deepEqual;
 
-/*
- * Fixtures.
- */
-
+/* Fixtures. */
 var lollipop = require('./fixtures/lollipop');
 var smile = require('./fixtures/smile');
 
-/**
- * Clone `object` but omit positional information.
- *
- * @param {Object|Array} object - Object to clone.
- * @return {Object|Array} - `object`, without positional
- *   information.
- */
-function clean(object) {
-    var clone = 'length' in object ? [] : {};
-    var key;
-    var value;
-
-    for (key in object) {
-        value = object[key];
-
-        if (key === 'position') {
-            continue;
-        }
-
-        clone[key] = typeof object[key] === 'object' ? clean(value) : value;
-    }
-
-    return clone;
-}
-
-/**
- * Add modifier to processor.
- */
-function plugin(processor) {
-    processor.Parser.prototype.useFirst('tokenizeSentence', emojiModifier);
-    processor.Parser.prototype.useFirst('tokenizeSentence', emoticonModifier);
-    processor.Parser.prototype.useFirst('tokenizeParagraph', modifier);
-}
-
-/*
- * Processors.
- */
-
+/* Processors. */
 var position = retext(english).use(plugin);
 var noPosition = retext(english).use(plugin).use(function (instance) {
-    instance.Parser.prototype.position = false;
+  instance.Parser.prototype.position = false;
 });
 
-/**
- * Short-cut to access the CST.
- */
+/* Short-cut to access the CST. */
 function process(fixture, processor) {
-    var cst;
+  var cst;
 
-    processor.process(fixture, function (err, file) {
-        /* istanbul ignore next */
-        if (err) {
-            throw err;
-        }
+  processor.process(fixture, function (err, file) {
+    /* istanbul ignore next */
+    if (err) {
+      throw err;
+    }
 
-        cst = file.namespace('retext').cst;
-    });
+    cst = file.namespace('retext').cst;
+  });
 
-    return cst;
-}
-
-/**
- * Short-cut to access the CST.
- */
-function check(fixture, node) {
-    dequal(process(fixture, position), node);
-    dequal(process(fixture, noPosition), clean(node));
+  return cst;
 }
 
 /*
@@ -100,14 +44,53 @@ function check(fixture, node) {
  */
 
 describe('nlcst-affix-emoticon-modifier()', function () {
-    it('should throw when not given a parent', function () {
-        assert.throws(function () {
-            modifier({});
-        }, /Missing children in `parent/);
-    });
+  it('should throw when not given a parent', function () {
+    assert.throws(
+      function () {
+        modifier({});
+      },
+      /Missing children in `parent`/
+    );
+  });
 
-    it('should merge at sentence-start', function () {
-        check('Lol! :lollipop: That’s cool.', lollipop);
-        check('Lol! :) That’s cool.', smile);
-    });
+  it('should merge at sentence-start', function () {
+    check('Lol! :lollipop: That’s cool.', lollipop);
+    check('Lol! :) That’s cool.', smile);
+  });
 });
+
+/**
+ * Short-cut to access the CST.
+ */
+function check(fixture, node) {
+  dequal(process(fixture, position), node);
+  dequal(process(fixture, noPosition), clean(node));
+}
+
+/**
+ * Add modifier to processor.
+ */
+function plugin(processor) {
+  processor.Parser.prototype.useFirst('tokenizeSentence', emojiModifier);
+  processor.Parser.prototype.useFirst('tokenizeSentence', emoticonModifier);
+  processor.Parser.prototype.useFirst('tokenizeParagraph', modifier);
+}
+
+/* Clone `object` but omit positional information. */
+function clean(object) {
+  var clone = 'length' in object ? [] : {};
+  var key;
+  var value;
+
+  for (key in object) {
+    value = object[key];
+
+    if (key === 'position') {
+      continue;
+    }
+
+    clone[key] = typeof object[key] === 'object' ? clean(value) : value;
+  }
+
+  return clone;
+}

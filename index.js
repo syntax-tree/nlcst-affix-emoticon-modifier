b560f18d62c9fe389b55589a5906b581277566a3
Titus Wormer <tituswormer@gmail.com>
2020-10-29 13:00:24 +0100

    Refactor to improve bundle size

--- index.js
+++ index.js
@@ -4,39 +4,35 @@ var modifier = require('unist-util-modify-children')
 
 module.exports = modifier(mergeAffixEmoticon)
 
-var emoticonNode = 'EmoticonNode'
-
 // Merge emoticons into an `EmoticonNode`.
 function mergeAffixEmoticon(child, index, parent) {
+  var siblings = parent.children
   var children = child.children
-  var position
-  var node
-  var previous
+  var childIndex = -1
 
-  if (children && children.length > 0 && index !== 0) {
-    position = -1
-
-    while (children[++position]) {
-      node = children[position]
-
-      if (node.type === emoticonNode) {
-        previous = parent.children[index - 1]
-
-        previous.children = previous.children.concat(
-          children.slice(0, position + 1)
+  if (children && children.length && index) {
+    while (++childIndex < children.length) {
+      if (children[childIndex].type === 'EmoticonNode') {
+        siblings[index - 1].children = [].concat(
+          siblings[index - 1].children,
+          children.slice(0, childIndex + 1)
         )
-        child.children = children.slice(position + 1)
+        child.children = children.slice(childIndex + 1)
 
-        if (node.position && child.position && previous.position) {
-          previous.position.end = node.position.end
-          child.position.start = node.position.end
+        if (
+          children[childIndex].position &&
+          child.position &&
+          siblings[index - 1].position
+        ) {
+          siblings[index - 1].position.end = children[childIndex].position.end
+          child.position.start = children[childIndex].position.end
         }
 
         // Next, iterate over the node again.
         return index
       }
 
-      if (node.type !== 'WhiteSpaceNode') {
+      if (children[childIndex].type !== 'WhiteSpaceNode') {
         break
       }
     }

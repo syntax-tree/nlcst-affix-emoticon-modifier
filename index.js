f4644972b893e61bece5f40ec9bbd76fbae61765
Titus Wormer <tituswormer@gmail.com>
2021-07-22 12:21:05 +0200

    Fix tests for changes in `@types/unist`

--- index.js
+++ index.js
@@ -10,42 +10,47 @@ export const affixEmoticonModifier = modifyChildren(mergeAffixEmoticon)
 /**
  * Merge emoticons into an `EmoticonNode`.
  *
- * @param {Node} child
+ * @param {Node} node
  * @param {number} index
- * @param {Parent} parent
+ * @param {Parent} ancestor
  */
-function mergeAffixEmoticon(child, index, parent) {
-  var siblings = parent.children
-  /** @type {Array.<Node>} */
-  // @ts-ignore looks like a parent.
-  var children = child.children
+function mergeAffixEmoticon(node, index, ancestor) {
+  const previous = ancestor.children[index - 1]
   var childIndex = -1
 
-  if (children && children.length > 0 && index) {
+  if (index && parent(previous) && parent(node)) {
+    var children = node.children
+
     while (++childIndex < children.length) {
-      if (children[childIndex].type === 'EmoticonNode') {
-        siblings[index - 1].children = [].concat(
-          siblings[index - 1].children,
+      const child = children[childIndex]
+
+      if (child.type === 'EmoticonNode') {
+        previous.children = [].concat(
+          previous.children,
           children.slice(0, childIndex + 1)
         )
-        child.children = children.slice(childIndex + 1)
+        node.children = children.slice(childIndex + 1)
 
-        if (
-          children[childIndex].position &&
-          child.position &&
-          siblings[index - 1].position
-        ) {
-          siblings[index - 1].position.end = children[childIndex].position.end
-          child.position.start = children[childIndex].position.end
+        if (child.position && node.position && previous.position) {
+          previous.position.end = child.position.end
+          node.position.start = child.position.end
         }
 
         // Next, iterate over the node again.
         return index
       }
 
-      if (children[childIndex].type !== 'WhiteSpaceNode') {
+      if (child.type !== 'WhiteSpaceNode') {
         break
       }
     }
   }
+}
+
+/**
+ * @param {Node} node
+ * @returns {node is Parent}
+ */
+function parent(node) {
+  return 'children' in node
 }

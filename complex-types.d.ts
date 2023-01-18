be69c9d53d79c086262e2f027484242d7947fee1
Titus Wormer <tituswormer@gmail.com>
2023-01-18 19:30:22 +0100

    Fix to expose the same emoticon node as others

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -1,13 +1,2 @@
-import type {Literal} from 'nlcst'
-
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-export interface Emoticon extends Literal {
-  type: 'EmoticonNode'
-}
-
-declare module 'nlcst' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  interface SentenceContentMap {
-    emoticon: Emoticon
-  }
-}
+// To do: remove next major.
+export type {Emoticon} from 'nlcst-emoticon-modifier'
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * @typedef {import('./complex-types.js').Emoticon} Emoticon
+ * @typedef {import('nlcst-emoticon-modifier').Emoticon} Emoticon
  */
 
 export {affixEmoticonModifier} from './lib/index.js'
